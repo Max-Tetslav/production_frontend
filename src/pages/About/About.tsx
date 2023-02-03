ce763f96f70a57e523a038f47f3e2a6fd8b5981a
Maksim Tetslav <maks.tetslav@yandex.ru>
2023-02-03 23:34:48 +0700

    feat: set up feature-slice-design methodology structure

--- src/pages/About/About.tsx
+++
@@ -1,8 +0,0 @@
-import React, { FC } from "react";
-import cls from "./About.module.scss";
-
-const About: FC = () => {
-  return <div className={cls.container}>ABOUT PAGE</div>;
-};
-
-export default About;

fb9fd0d08f58f00fccfa72c0712a0521867c6930
Maksim Tetslav <maks.tetslav@yandex.ru>
2023-02-12 04:25:37 +0700

    feat: add not found page component

--- src/app/router/config/router.tsx
+++ src/app/router/config/router.tsx
@@ -1,15 +1,18 @@
 import { RouteProps } from 'react-router-dom';
 
 import { About, Main } from 'pages';
+import { PageNotFound } from 'widgets/PageNotFound';
 
 export enum EAppRoutes {
-  MAIN = 'main',
-  ABOUT = 'about',
+    MAIN = 'main',
+    ABOUT = 'about',
+    NOT_FOUND = 'not_found'
 }
 
 export const routesPathList: Record<EAppRoutes, string> = {
     [EAppRoutes.MAIN]: '/',
     [EAppRoutes.ABOUT]: '/about',
+    [EAppRoutes.NOT_FOUND]: '*',
 };
 
 export const routeList: Record<EAppRoutes, RouteProps> = {
@@ -20,5 +23,9 @@ export const routeList: Record<EAppRoutes, RouteProps> = {
     [EAppRoutes.ABOUT]: {
         path: routesPathList[EAppRoutes.ABOUT],
         element: <About />,
+    },
+    [EAppRoutes.NOT_FOUND]: {
+        path: routesPathList[EAppRoutes.NOT_FOUND],
+        element: <PageNotFound />,
     },
 };

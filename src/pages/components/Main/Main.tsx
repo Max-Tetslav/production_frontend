a0a9f7d05832e39d8f6aff9fbcf0a5e30000a565
Maksim Tetslav <maks.tetslav@yandex.ru>
2023-02-23 06:29:24 +0700

    refactor: remove unused imports

--- src/pages/components/Main/Main.tsx
+++ src/pages/components/Main/Main.tsx
@@ -1,8 +1,7 @@
-import BugButton from 'features/BugButton/BugButton';
-import React, { FC } from 'react';
-import { useTranslation } from 'react-i18next';
+import { FC } from 'react';
 
-import user from 'shared/assets/user.png';
+import { useTranslation } from 'react-i18next';
+import BugButton from 'features/BugButton/BugButton';
 import cls from './Main.module.scss';
 
 const Main: FC = () => {

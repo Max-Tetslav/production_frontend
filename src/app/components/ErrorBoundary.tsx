3c7eb5daae958b8ebbda31ce27c12c32db4b6c5d
Maksim Tetslav <maks.tetslav@yandex.ru>
2023-02-23 06:24:24 +0700

    refactor: apply prettier and minor changes

--- src/app/components/ErrorBoundary.tsx
+++ src/app/components/ErrorBoundary.tsx
@@ -9,22 +9,17 @@ interface IErrorBoundaryState {
     hasError: boolean;
 }
 
-class ErrorBoundary extends Component<
-    IErrorBoundaryProps,
-    IErrorBoundaryState
-> {
+class ErrorBoundary extends Component<IErrorBoundaryProps, IErrorBoundaryState> {
     constructor(props: IErrorBoundaryProps) {
         super(props);
         this.state = { hasError: false };
     }
 
     static getDerivedStateFromError(error: Error) {
-        // Update state so the next render will show the fallback UI.
         return { hasError: true };
     }
 
     componentDidCatch(error: Error, errorInfo: ErrorInfo) {
-        // You can also log the error to an error reporting service
         console.log(error, errorInfo);
     }
 
@@ -33,7 +28,6 @@ class ErrorBoundary extends Component<
         const { children } = this.props;
 
         if (hasError) {
-            // You can render any custom fallback UI
             return <ErrorBoundaryFallback />;
         }
 

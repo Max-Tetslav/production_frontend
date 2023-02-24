5b0e85a86a666836fd4489ba9ccde040d69fe04d
Maksim Tetslav <maks.tetslav@yandex.ru>
2023-02-25 02:50:14 +0700

    feat: update renderWithProvider by store provider

--- src/shared/lib/helpers/renderWithProviders.tsx
+++ src/shared/lib/helpers/renderWithProviders.tsx
@@ -3,21 +3,25 @@ import { MemoryRouter } from 'react-router-dom';
 import { I18nextProvider } from 'react-i18next';
 import { render } from '@testing-library/react';
 
+import { StoreProvider } from 'app/providers/Store';
+import { DeepPartial } from 'redux';
 import i18nForTests from '../config/i18n/i18nForTests';
+import { StateScheme } from '../config/store';
 
 export interface IRenderComponentOptions {
     route?: string;
+    initialState?: DeepPartial<StateScheme>;
 }
 
 const renderWithProviders = (component: ReactNode, options: IRenderComponentOptions = {}) => {
-    const { route = '/' } = options;
+    const { route = '/', initialState } = options;
 
     return render(
-        <MemoryRouter initialEntries={[route]}>
-            <I18nextProvider i18n={i18nForTests}>
-                {component}
-            </I18nextProvider>
-        </MemoryRouter>,
+        <StoreProvider initialState={initialState}>
+            <MemoryRouter initialEntries={[route]}>
+                <I18nextProvider i18n={i18nForTests}>{component}</I18nextProvider>
+            </MemoryRouter>
+        </StoreProvider>,
     );
 };
 
